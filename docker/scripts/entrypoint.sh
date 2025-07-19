0738dd940d5f59a922b57e85d6737dbda51fb13d
david_vl2003@hotmail.com <david_vl2003@hotmail.com>
2025-07-18 19:08:31 -0600

    Experimental: Use of entrypoint.sh for runtime operations

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 set -e
 
-echo "Running rosdep update and rosdep install for mounted workspace..."
+# Source ROS
+source /opt/ros/humble/setup.bash
 
+#rosdep
 rosdep update
 rosdep install --from-paths /ros/home_base_ws/src --ignore-src -r -y
 
-exec "$@"
+# Optional: source overlay if needed
+if [ -f "/ros/home_base_ws/install/setup.bash" ]; then
+  source /ros/home_base_ws/install/setup.bash
+fi
+
+# Build
+cd /ros/home_base_ws
+colcon build --symlink-install
+
+# Keep container alive interactively if no other command was passed
+if [ "$#" -eq 0 ]; then
+  exec bash
+else
+  exec "$@"
+fi
